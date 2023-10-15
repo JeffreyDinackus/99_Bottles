28593b846cee7d14b4b267a31d36b2b71cba0876
JeffreyDinackus <jeffreydinackus@proton.me>
2023-10-15 15:53:10 -0400

    lyrics corrected

--- 99_bottles.py
+++ 99_bottles.py
@@ -2,4 +2,5 @@ N = 99
 
 for num in reversed(range(N + 1)) : 
   
-  print("There are {} bottles of beer on the wall! {} bottles of beer on the wall!".format(num,num))
+  print("{} bottles of beer on the wall! {} bottles of beer on the wall!".format(num,num))
+  print("Take one down pass it around!")
